6f99940d321e9c337b57f6c5a5c5dd6888dbd6d7
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-12-11 15:13:41 +0100

    2nd commit

--- testdriven-app/services/users/project/config.py
+++ testdriven-app/services/users/project/config.py
@@ -1,21 +1,27 @@
 # services/users/project/config.py
 
 
+import os  # new
+
+
 class BaseConfig:
     """Base configuration"""
     TESTING = False
+    SECRET_KEY = 'my_precious'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
 
 
 class DevelopmentConfig(BaseConfig):
     """Development configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
 
 
 class TestingConfig(BaseConfig):
     """Testing configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
