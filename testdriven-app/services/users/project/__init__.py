f82894af79248e0e97c0ff76edae6a7eeaa091dc
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-12-12 15:52:33 +0100

    new com

--- testdriven-app/services/users/project/__init__.py
+++ testdriven-app/services/users/project/__init__.py
@@ -5,13 +5,14 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_debugtoolbar import DebugToolbarExtension  # new
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_cors import CORS
-
+from flask_migrate import Migrate  # new
 
 # instantiate the extensions
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()  # new
+migrate = Migrate()
 
 
 def create_app(script_info=None):
@@ -29,6 +30,7 @@ def create_app(script_info=None):
     # set up extensions
     db.init_app(app)
     toolbar.init_app(app)
+    migrate.init_app(app, db)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
