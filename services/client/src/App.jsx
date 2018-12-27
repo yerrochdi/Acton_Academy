25285080bdd2c9758b1efbde9a96d913edfa9bf8
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-12-27 14:49:30 +0100

    authentif

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -7,6 +7,7 @@ import Form from './components/Form';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import Logout from './components/Logout';
 
 
 class App extends Component {
@@ -23,9 +24,14 @@ class App extends Component {
         email: '',
         password: ''
       },
+      isAuthenticated: false,  // new
     };
     this.addUser = this.addUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.logoutUser = this.logoutUser.bind(this);
+
   };
   componentDidMount() {
     this.getUsers();
@@ -52,6 +58,42 @@ class App extends Component {
     const obj = {};
     obj[event.target.name] = event.target.value;
     this.setState(obj);
+  };
+  handleUserFormSubmit(event) {
+    event.preventDefault();
+    const formType = window.location.href.split('/').reverse()[0];
+    let data = {
+      email: this.state.formData.email,
+      password: this.state.formData.password,
+    };
+    if (formType === 'register') {
+      data.username = this.state.formData.username;
+    }
+    const url = `${process.env.REACT_APP_USERS_SERVICE_URL}/auth/${formType}`
+    axios.post(url, data)
+    .then((res) => {
+      this.clearFormState();  // new
+      window.localStorage.setItem('authToken', res.data.auth_token);  // new
+      this.setState({ isAuthenticated: true, });  // new
+      this.getUsers();
+    })
+    .catch((err) => { console.log(err); });
+  };
+  handleFormChange(event) {
+    const obj = this.state.formData;
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  };
+  clearFormState() {
+    this.setState({
+      formData: { username: '', email: '', password: '' },
+      username: '',
+      email: ''
+      });
+  };
+  logoutUser() {
+    window.localStorage.clear();
+    this.setState({ isAuthenticated: false });
   };
   render() {
     return (
@@ -82,14 +124,26 @@ class App extends Component {
                     <Form
                       formType={'Register'}
                       formData={this.state.formData}
+                      handleUserFormSubmit={this.handleUserFormSubmit}  // new
+                      handleFormChange={this.handleFormChange}
+                      isAuthenticated={this.state.isAuthenticated}  // new
                     />
                   )} />
                   <Route exact path='/login' render={() => (
                     <Form
                       formType={'Login'}
                       formData={this.state.formData}
+                      handleUserFormSubmit={this.handleUserFormSubmit}  // new
+                      handleFormChange={this.handleFormChange}
+                      isAuthenticated={this.state.isAuthenticated}  // new
                     />
                   )} />
+                  <Route exact path='/logout' render={() => (
+                      <Logout
+                        logoutUser={this.logoutUser}
+                        isAuthenticated={this.state.isAuthenticated}
+                      />
+                    )} />
                 </Switch>
               </div>
             </div>
