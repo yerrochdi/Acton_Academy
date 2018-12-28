4ecbef7778856f90a16f617e63dc830f70cb1b22
yerrochdi <yassine.errochdi@telecom-paristech.fr>
2018-12-28 15:13:20 +0100

    toto

---
+++ cypress/integration/index.test.js
@@ -0,0 +1,9 @@
+describe('Index', () => {
+
+  it('users should be able to view the "/" page', () => {
+    cy
+      .visit('localhost/')
+      .get('h1').contains('All Users');
+  });
+
+});
--- test.sh
+++ test.sh
@@ -19,6 +19,14 @@ docker-compose -f docker-compose-dev.yml run client npm test -- --coverage
 inspect $? client
 docker-compose -f docker-compose-dev.yml down
 
+# new
+# run e2e tests
+docker-compose -f docker-compose-prod.yml up -d --build
+docker-compose -f docker-compose-prod.yml run users python manage.py recreate_db
+./node_modules/.bin/cypress run --config baseUrl=http://localhost
+inspect $? e2e
+docker-compose -f docker-compose-prod.yml down
+
 # return proper code
 if [ -n "${fails}" ]; then
   echo "Tests failed: ${fails}"
